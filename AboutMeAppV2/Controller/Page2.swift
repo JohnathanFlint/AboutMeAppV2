6966a6a6a82457641b1e4d4d85595e5cf4405ecf
JohnathanFlint <nicholas.white142@gmail.com>
2016-10-11 08:27:33 -0600

    Created the AboutMeV2 project.

---
+++ AboutMeAppV2/Controller/Page2.swift
@@ -0,0 +1,14 @@
+//
+//  Page2.swift
+//  AboutMeAppV2
+//
+//  Created by White, Nicholas on 10/11/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class Page2 : UIViewController
+{
+    
+}
